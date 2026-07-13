0fbe5c32490302a0037e9a54eed859eac4323902
Anurag Kumar <ak7546824@gmail.com>
2026-07-13 23:51:16 +0530

    Create SearchBar.jsx

---
+++ client/src/components/SearchBar.jsx
@@ -0,0 +1,33 @@
+function SearchBar({
+
+    value,
+
+    onChange,
+
+}) {
+
+    return (
+
+        <div className="search-bar">
+
+            <input
+
+                type="text"
+
+                placeholder="Search users..."
+
+                value={value}
+
+                onChange={(e) =>
+                    onChange(e.target.value)
+                }
+
+            />
+
+        </div>
+
+    );
+
+}
+
+export default SearchBar;
