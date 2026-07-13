9c8103e24b4e9090f54a24eb1a1874bfab0f9a10
Anurag Kumar <ak7546824@gmail.com>
2026-07-13 23:51:49 +0530

    Create user.controller.js

---
+++ server/controllers/user.controller.js
@@ -0,0 +1,17 @@
+import User from "../models/User.js";
+
+export const getUsers = async (req, res) => {
+    try {
+        const users = await User.find({
+            _id: {
+                $ne: req.user._id,
+            },
+        }).select("-password");
+
+        res.json(users);
+    } catch (err) {
+        res.status(500).json({
+            message: err.message,
+        });
+    }
+};
